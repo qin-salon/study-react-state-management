d4eb6b9c331b21b4fd35f341c3faaa6fe97e358c
skoto <0120koto@gmail.com>
2022-06-15 14:53:11 +0900

    ✨ Zustand02の準備

---
+++ Zustand02/src/pages/add.tsx
@@ -0,0 +1,26 @@
+import type { NextPage } from "next";
+import { ComponentProps } from "react";
+import { useStore } from "src/state/todos";
+
+const Add: NextPage = () => {
+  const addTodo = useStore((state) => state.addTodo);
+
+  const handleSubmit: ComponentProps<"form">["onSubmit"] = (event) => {
+    event.preventDefault();
+    const text = event.currentTarget.text.value;
+    addTodo(text);
+    event.currentTarget.reset();
+  };
+
+  return (
+    <div>
+      <h3>TODO追加</h3>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="text" autoComplete="off" required />
+        <button>追加</button>
+      </form>
+    </div>
+  );
+};
+
+export default Add;
